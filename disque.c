32b5b4dcf29183b665fcaaeef7a61ce8fe0e16fb
Joseph DALY <joseph300905@gmail.com>
2024-08-23 21:22:59 +0200

    create helper http function

--- disque.c
+++ disque.c
@@ -29,51 +29,62 @@ write_callback(void *contents, size_t size, size_t nmemb, void *userp)
   return realsize;
 }
 
-struct DisqueUser *
-disque_get_current_user(struct DisqueContext *ctx)
+char *
+disque_request(CURL *curl, struct DisqueContext *ctx)
 {
-  CURL *curl;
-  CURLcode res;
+  CURLcode code;
   struct curl_slist *headers = NULL;
   char header[128];
-  struct ResponseBody response;
-  struct DisqueUser *user = malloc(sizeof(struct DisqueUser));
+  struct ResponseBody res;
 
-  curl_global_init(CURL_GLOBAL_ALL);
-
-  curl = curl_easy_init();
-  if (!curl) return NULL;
-
-  response.content = malloc(1);
-  response.size = 0;
+  res.content = malloc(1);
+  res.size = 0;
 
   strcpy(header, "Authorization: Bot ");
   strcat(header, ctx->token);
 
   headers = curl_slist_append(headers, header);
 
-  curl_easy_setopt(curl, CURLOPT_URL, "https://discord.com/api/v10/users/@me");
   curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
   curl_easy_setopt(curl, CURLOPT_CA_CACHE_TIMEOUT, 604800);
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &response);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &res);
 
-  res = curl_easy_perform(curl);
-
-  if (CURLE_OK != res) return NULL;
+  code = curl_easy_perform(curl);
 
   curl_easy_cleanup(curl);
   curl_slist_free_all(headers);
 
-  cJSON *json = cJSON_Parse(response.content);
+  if (CURLE_OK != code)
+    return NULL;
+
+  return res.content;
+}
+
+struct DisqueUser *
+disque_get_current_user(struct DisqueContext *ctx)
+{
+  CURL *curl;
+  char *response;
+  struct DisqueUser *user;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+  curl = curl_easy_init();
+  if (!curl)
+    return NULL;
+  curl_easy_setopt(curl, CURLOPT_URL, "https://discord.com/api/v10/users/@me");
+  response = disque_request(curl, ctx);
+
+  cJSON *json = cJSON_Parse(response);
   cJSON *username = cJSON_GetObjectItemCaseSensitive(json, "username");
 
+  user = malloc(sizeof(struct DisqueUser));
   strcpy(user->username, username->valuestring);
 
   cJSON_Delete(json);
-  free(response.content);
+  free(response);
 
   return user;
 }
